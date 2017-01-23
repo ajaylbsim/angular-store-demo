e094fd58adaab19dae4de5985b2b99a299e4475f
ajaylbsim@gmail.com <ajaylbsim@gmail.com>
2017-01-23 16:09:23 +0530

    api sever port changed

--- server.js
+++ server.js
@@ -96,6 +96,6 @@ app.post('/item/detail', function (req, res) {
 
 })
 
-app.listen(3001, function () {
-	console.log('Example app listening on port 3001!')
+app.listen(3003, function () {
+	console.log('Example app listening on port 3003!')
 })
