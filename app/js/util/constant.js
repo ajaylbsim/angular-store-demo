4f391aaeb6fc8247d3a5af928204fbad2eea4394
ajaylbsim@gmail.com <ajaylbsim@gmail.com>
2017-01-23 16:20:29 +0530

    api changes

--- app/js/util/constant.js
+++ app/js/util/constant.js
@@ -1,6 +1,6 @@
 
 	var ME = 'http://localhost:3003';
-	var API_SERVER "http://ec2-52-77-84-179.ap-southeast-1.compute.amazonaws.com:3003"
+	var API_SERVER = "http://ec2-52-77-84-179.ap-southeast-1.compute.amazonaws.com:3003"
 	angular.module('app').constant('Constants', {
 	    APP_URL:API_SERVER
 	});
